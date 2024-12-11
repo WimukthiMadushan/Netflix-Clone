3e4d90c0acf9cafa95d47c027336a3318dd88b6c
WimukthiMdaushan <bandarawgwm.21@uom.lk>
2024-12-11 08:59:23 +0530

    complete navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+import profile_icon from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
 import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <div className='navbar'>NavBar</div>
+    <div className='navbar'>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse By Lanuage</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} alt="" className='icons'/>
+        <p>Children</p>
+        <img src={bell_icon} alt="" className='icons'/>
+        <div className='navbar-profile'>
+          <img src={profile_icon} alt="" className='profile'/>
+          <img src={caret_icon} alt=""/>
+          <div className="dropdown">
+            <p>SignOut of Netflix</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
